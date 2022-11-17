6345ddca4285c79ea2dd3c35aac228cfb61fbf72
otkis <107149087+otkisider@users.noreply.github.com>
2022-11-17 11:55:24 +0300

    lab2v2

--- lab2/programlab2/Program.cs
+++ lab2/programlab2/Program.cs
@@ -1,135 +1,139 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Numerics;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
-public class Typeofworks
+
+namespace Serialization
 {
-    public int Order_number { get; private set; }
-    public int Job_line_number { get; private set; }
-    public string Value { get; private set; }
-
-    public int Job_id { get; private set; }
-
-    public Workcatalog workcatalog { get; private set; }
-
-    public Typeofworks() { }
-
-    public Typeofworks(int Order_number, int Job_line_number, string Value, int Job_id, Workcatalog workcatalog)
+    public class Client
     {
-        this.Order_number = Order_number;
-        this.Job_line_number = Job_line_number;
-        this.Value = Value;
-        this.Job_id = Job_id;
-        this.workcatalog = workcatalog;
-    }
-}
-
-public class Workcatalog
-{
-    public int Job_id { get; private set; }
-    public string Name { get; private set; }
-    public int price { get; private set; }
-
-    public Workcatalog() { }
-
-    public Workcatalog(int Job_id, string Name, int price)
-    {
-        this.Job_id = Job_id;
-        this.Name = Name;
-        this.price = price;
-    }
-}
-
-
-
-public class JsonHandler<T> where T : class
-{
-    private string fileName;
-    JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
-
-
-    public JsonHandler() { }
-
-    public JsonHandler(string fileName)
-    {
-        this.fileName = fileName;
-    }
-
-
-    public void SetFileName(string fileName)
-    {
-        this.fileName = fileName;
-    }
-
-    public void Write(List<T> list)
-    {
-        string jsonString = JsonSerializer.Serialize(list, options);
-
-        if (new FileInfo(fileName).Length == 0)
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Scnd_name { get; set; }
+        public long Phone { get; set; }
+        public Call ID_ivr { get; set; }
+        public Client() { }
+        public Client(int id, string name, string scnd_name, long phone, Call id_ivr)
         {
-            File.WriteAllText(fileName, jsonString);
+            Id = id;
+            Name = name;
+            Scnd_name = scnd_name;
+            Phone = phone;
+            ID_ivr = id_ivr;
         }
-        else
-        {
-            Console.WriteLine("Specified path file is not empty");
-        }
-    }
 
-    public void Delete()
-    {
-        File.WriteAllText(fileName, string.Empty);
-    }
 
-    public void Rewrite(List<T> list)
-    {
-        string jsonString = JsonSerializer.Serialize(list, options);
 
-        File.WriteAllText(fileName, jsonString);
-    }
-
-    public void Read(ref List<T> list)
-    {
-        if (File.Exists(fileName))
-        {
-            if (new FileInfo(fileName).Length != 0)
+        public class Call
+        {   
+            public int ID_ivr { get; set; }        
+            public int Credit_limit { get; set; }
+            public int Current_credit { get; set; }
+            public Call() { }
+            public Call(int id_ivr, int credit_limit, int current_credit)
             {
-                string jsonString = File.ReadAllText(fileName);
-                list = JsonSerializer.Deserialize<List<T>>(jsonString);
+                ID_ivr = id_ivr;
+                Credit_limit = credit_limit;
+                Current_credit = current_credit;
+            
+            }
+
+        }
+    }
+
+    public class JsonHandler<T> where T : class
+    {
+        private string NameFile;
+        JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+
+
+        public JsonHandler() { }
+
+        public JsonHandler(string NameFile)
+        {
+            this.NameFile = NameFile;
+        }
+
+
+        public void SetFileName(string NameFile)
+        {
+            this.NameFile = NameFile;
+        }
+
+        public void Write(List<T> list)
+        {
+            string jsonString = JsonSerializer.Serialize(list, options);
+
+            if (new FileInfo(NameFile).Length == 0)
+            {
+                File.WriteAllText(NameFile, jsonString);
             }
             else
             {
-                Console.WriteLine("Specified path file is empty");
+                Console.WriteLine("Указанный путь к файлу не пустой");
             }
+        }
+
+        public void Delete()
+        {
+            File.WriteAllText(NameFile, string.Empty);
+        }
+
+        public void Rewrite(List<T> list)
+        {
+            string jsonString = JsonSerializer.Serialize(list, options);
+
+            File.WriteAllText(NameFile, jsonString);
+        }
+
+        public void Read(ref List<T> list)
+        {
+            if (File.Exists(NameFile))
+            {
+                if (new FileInfo(NameFile).Length != 0)
+                {
+                    string jsonString = File.ReadAllText(NameFile);
+                    list = JsonSerializer.Deserialize<List<T>>(jsonString);
+                }
+                else
+                {
+                    Console.WriteLine("Указанный путь к файлу не пустой");
+                }
+            }
+        }
+
+        public void OutputJsonContents()
+        {
+            string jsonString = File.ReadAllText(NameFile);
+
+            Console.WriteLine(jsonString);
+        }
+
+        public void OutputSerializedList(List<T> list)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(list, options));
         }
     }
 
-    public void OutputJsonContents()
+   public class Program
     {
-        string jsonString = File.ReadAllText(fileName);
+        static void Main(string[] args)
+        {
+            List<Client> partsList = new List<Client>();
 
-        Console.WriteLine(jsonString);
-    }
+            JsonHandler<Client> partsHandler = new JsonHandler<Client>("FileClient.json");
 
-    public void OutputSerializedList(List<T> list)
-    {
-        Console.WriteLine(JsonSerializer.Serialize(list, options));
-    }
-}
+            partsList.Add(new Client(100, "Kiril", "Bolshoy", 89103834323, new Client.Call(15, 123123123, 15151515)));
+            partsList.Add(new Client(100, "Kiril", "Bolshoy", 89103834323, new Client.Call(15, 123123123, 15151515)));
+            partsList.Add(new Client(100, "Kiril", "Bolshoy", 89103834323, new Client.Call(15, 123123123, 15151515)));
+            partsList.Add(new Client(100, "Kiril", "Bolshoy", 89103834323, new Client.Call(15, 123123123, 15151515)));
+            partsList.Add(new Client(100, "Kiril", "Bolshoy", 89103834323, new Client.Call(15, 123123123, 15151515)));
 
-
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        List<Typeofworks> partsList = new List<Typeofworks>();
-
-        JsonHandler<Typeofworks> partsHandler = new JsonHandler<Typeofworks>("partsFile.json");
-
-        partsList.Add(new Typeofworks(1, 1, " ", 1, new Workcatalog(1, "Alignment walls", 1000)));
-        partsList.Add(new Typeofworks(1, 2, " ", 2, new Workcatalog(2, "Wallpaper Pasting", 650)));
-        partsList.Add(new Typeofworks(2, 1, "Square meters", 3, new Workcatalog(3, "Laying tiles", 2000)));
-        partsList.Add(new Typeofworks(2, 2, "Meters", 4, new Workcatalog(4, "Communication plumbing", 400)));
-        partsHandler.Rewrite(partsList);
-        partsHandler.OutputJsonContents();
+            partsHandler.Rewrite(partsList);
+            partsHandler.OutputJsonContents();
+        }
     }
 }
